4c8be1b07480d45e3ec14cecebfcc7e8b8240a91
fran <fragonzcuriel@gmail.com>
2022-11-27 23:24:51 +0100

    llamadas a la base de datos para obtener el ranking

--- src/main/java/com/zetcode/Gestores/Gestor_ranking.java
+++ src/main/java/com/zetcode/Gestores/Gestor_ranking.java
@@ -1,31 +1,66 @@
 package com.zetcode.Gestores;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Gestor_ranking {
     private static Gestor_ranking miGestorRanking;
-    private Gestor_ranking(){}
+    private Gestor_ranking(){
+
+    }
     public static Gestor_ranking getmiGestorRanking(){
         if(miGestorRanking ==null){
             miGestorRanking =new Gestor_ranking();
         }
         return miGestorRanking;
     }
-    public ArrayList<String> getlistaRanking(int tipo){
-
-        
-        ArrayList<String> lista = new ArrayList<>();
-        if(tipo==1){
-            for(int i=0;i<1000;i++){
-            lista.add("Hola");
+    
+    public JSONArray generarRanking(String dificultad){
+        Connection con = null;
+        String sURL = "jdbc:mariadb://localhost:3306/ADSI";
+        JSONArray jsonArray = new JSONArray(); 
+        try {
+          con = DriverManager.getConnection(sURL,"root","");
+          try (
+            PreparedStatement query = con.prepareStatement("SELECT user, puntuacion from info_ranking where dificultad = ?")) {
+                query.setString(1, dificultad);    
+            // Ejecutamos Query
+            ResultSet rs = query.executeQuery();
+            
+            // Recorremos el resultado
+            while (rs.next()){
+                int columns = rs.getMetaData().getColumnCount();
+                JSONObject obj = new JSONObject();
+         
+                for (int i = 0; i < columns; i++)
+                    obj.put(rs.getMetaData().getColumnLabel(i + 1).toLowerCase(), rs.getObject(i + 1));
+         
+                jsonArray.put(obj);
             }
+              
+            }catch (SQLException sqle) { 
+              System.out.println("Error en la ejecución:" 
+            + sqle.getErrorCode() + " " + sqle.getMessage());    
+            }
+        } catch (Exception e) { 
+          System.out.println("Error en la conexión:" + e.toString() );
+        } finally {
+          try {
+            // Cerramos posibles conexiones abiertas
+            if (con!=null) con.close();    
+          } catch (Exception e) {
+            System.out.println("Error cerrando conexiones: "
+              + e.toString());
+          } 
         }
-        else{
-            for(int i=0;i<1000;i++){
-                lista.add("ADios");
-                }
-        }
-       
-       return lista;
+        return jsonArray;
     }
+    
 }
