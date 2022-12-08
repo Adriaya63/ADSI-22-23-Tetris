3ee9a82803b46d6c850049fcbd5a7e6ff035a379
Joanaldi <70902423+Joanaldi@users.noreply.github.com>
2022-12-08 19:01:18 +0100

    Menu Registro/inicio

--- src/main/java/com/zetcode/Extensiones/MenuRegistroInicio.java
+++ src/main/java/com/zetcode/Extensiones/MenuRegistroInicio.java
@@ -6,10 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
 
 public class MenuRegistroInicio extends JFrame {
 
 	private JPanel contentPane;
+	private JPanel panel;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	private JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -34,9 +44,45 @@ public class MenuRegistroInicio extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(51, 204, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.add(getPanel(), BorderLayout.CENTER);
 	}
 
+	private JPanel getPanel() {
+		if (panel == null) {
+			panel = new JPanel();
+			panel.setBackground(new Color(0, 204, 255));
+			panel.setLayout(null);
+			panel.add(getBtnNewButton());
+			panel.add(getBtnNewButton_1());
+			panel.add(getLblNewLabel());
+		}
+		return panel;
+	}
+	private JButton getBtnNewButton() {
+		if (btnNewButton == null) {
+			btnNewButton = new JButton("INICIA SESI\u00D3N");
+			btnNewButton.setBounds(10, 163, 193, 56);
+		}
+		return btnNewButton;
+	}
+	private JButton getBtnNewButton_1() {
+		if (btnNewButton_1 == null) {
+			btnNewButton_1 = new JButton("REGISTRATE");
+			btnNewButton_1.setBounds(213, 163, 203, 56);
+		}
+		return btnNewButton_1;
+	}
+	private JLabel getLblNewLabel() {
+		if (lblNewLabel == null) {
+			lblNewLabel = new JLabel("TETRIS");
+			lblNewLabel.setFont(new Font("Imprint MT Shadow", Font.BOLD | Font.ITALIC, 25));
+			lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			lblNewLabel.setBounds(10, 51, 406, 75);
+		}
+		return lblNewLabel;
+	}
 }
