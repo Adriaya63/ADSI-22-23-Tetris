6de7e42619a6bdb14426e728d440645cf68a3ab2
Adriaya63 <Adriaya63@gmail.com>
2022-12-08 19:02:00 +0100

    Colores cambiados

--- src/main/java/com/zetcode/Gestores/Gestor_Personalizacion.java
+++ src/main/java/com/zetcode/Gestores/Gestor_Personalizacion.java
@@ -10,10 +10,10 @@ public class Gestor_Personalizacion {
         new Color(204, 204, 102), new Color(204, 102, 204),
         new Color(102, 204, 204), new Color(218, 170, 0)
     };
-    private Color colorsClasic[] = { new Color(0, 0, 0), new Color(255, 255, 255),
+    private Color colorsClasic[] = { new Color(0, 0, 0), new Color(255, 0, 0),
+        new Color(0, 0, 255), new Color(255, 255, 255),
         new Color(255, 255, 255), new Color(255, 255, 255),
-        new Color(255, 255, 255), new Color(255, 255, 255),
-        new Color(255, 255, 255), new Color(255, 255, 255)
+        new Color(255, 0, 0), new Color(0, 0, 255)
     };
     private Color colorsBlack[] = { new Color(0, 0, 0), new Color(10, 10, 10),
         new Color(10, 10, 10), new Color(10, 10, 10),
